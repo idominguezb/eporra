6c1ca95965f0f04ee078cff50f10206149a79f01
Ismael Dominguez <ismaeldb_@hotmail.com>
2022-02-23 20:26:03 -0500

    add apostador model

---
+++ src/modelo/apostador.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+from .declarative_base import Base
+
+class Apostador(Base):
+    __tablename__ = 'apostadores'
+
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String)
